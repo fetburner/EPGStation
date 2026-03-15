796592da3067ad951998eddce134ab8ac17b32c9
Masayuki Mizuno <mizuno@wantedly.com>
2026-03-15 19:47:24 +0900

    Replace path.resolve(__dirname, '..') with process.cwd()

--- src/projectRoot.ts
+++ src/projectRoot.ts
@@ -1,6 +1,2 @@
-import * as path from 'path';
-// webpack 設定で node.__dirname = false とするため、実行時の __dirname は
-// バンドルファイルのディレクトリ（= dist/）を指す。
-// dist/ の親がプロジェクトルート。
-const PROJECT_ROOT = path.resolve(__dirname, '..');
+const PROJECT_ROOT = process.cwd();
 export default PROJECT_ROOT;
